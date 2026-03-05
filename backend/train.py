3c4ace8323b584cbbc86183874dbc0fa093920d7
ramisettynavya <rnavya2006@gmail.com>
2026-03-05 10:37:26 +0530

    svm model trained with sms spam dataset

--- backend/train.py
+++ backend/train.py
@@ -1,28 +1,50 @@
 import pandas as pd
 import joblib
-import os
 
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.svm import LinearSVC
+from sklearn.metrics import accuracy_score
 
-data = pd.read_csv("fraud_dataset.csv")
+# Load dataset
+df = pd.read_csv("spam.csv", encoding="latin-1")
 
-X = data["message"]
-y = data["label"]
+# Keep only required columns
+df = df[['v1', 'v2']]
+df.columns = ['label', 'message']
 
-vectorizer = TfidfVectorizer(ngram_range=(1,2))
-X_vec = vectorizer.fit_transform(X)
+# Convert labels to numbers
+df['label'] = df['label'].map({'ham': 0, 'spam': 1})
 
-model = RandomForestClassifier(
-    n_estimators=200,
-    random_state=42
+# Split features and labels
+X = df['message']
+y = df['label']
+
+# Convert text → numerical features
+vectorizer = TfidfVectorizer(stop_words='english')
+
+X_vectorized = vectorizer.fit_transform(X)
+
+# Train test split
+X_train, X_test, y_train, y_test = train_test_split(
+    X_vectorized, y, test_size=0.2, random_state=42
 )
 
-model.fit(X_vec, y)
+# Train SVM model
+model = LinearSVC()
 
-os.makedirs("saved_model", exist_ok=True)
+model.fit(X_train, y_train)
 
+# Predictions
+predictions = model.predict(X_test)
+
+# Accuracy
+accuracy = accuracy_score(y_test, predictions)
+
+print("Model Accuracy:", accuracy)
+
+# Save trained model
 joblib.dump(model, "saved_model/model.pkl")
 joblib.dump(vectorizer, "saved_model/vectorizer.pkl")
 
-print("Model trained successfully")
+print("Model trained and saved successfully!")
